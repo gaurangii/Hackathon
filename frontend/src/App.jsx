d9db66d1f83a38fa4c98ac10fec980b687df4dcf
gaurangii <156580660+gaurangii@users.noreply.github.com>
2025-02-22 10:25:51 +0530

    final changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Homepage from './components/Homepage'
-<<<<<<< HEAD
 import Upload from "./components/upload";
-import AboutUs from "./components/Aboutus";
-import ContactUs from "./components/Contact";
-=======
-import Upload from "./components/Upload.jsx";
-import Job from "./components/jobs";
->>>>>>> b1108ccdcf27deb937a47fa564ea92c9b381bd71
+
 
 function App() {
   
@@ -18,12 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/upload" element={<Upload />} />
-<<<<<<< HEAD
-        <Route path="/about-us" element={<AboutUs/>} />
-        <Route path="/contact-us" element={<ContactUs/>} />
-=======
-        <Route path="/jobs" element={<Job />} />
->>>>>>> b1108ccdcf27deb937a47fa564ea92c9b381bd71
+        {/* <Route path="/jobs" element={<Job />} /> */}
       </Routes>
     </Router>
   );
